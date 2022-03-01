b9f25ac876f5a41366ca1c414bbe726a37d22bd3
rafaelarissa <rafaela.larissalj@gmail.com>
2022-02-28 22:02:42 -0300

    chore: added .env file

---
+++ src/database.js
@@ -0,0 +1,10 @@
+import { Express } from 'express';
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default connection;

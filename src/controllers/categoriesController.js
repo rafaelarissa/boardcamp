1a54691e80a0a88484b84998655c621941cd9d69
rafaelarissa <rafaela.larissalj@gmail.com>
2022-03-02 13:56:31 -0300

    feat: /post categories creating new categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,11 +1,28 @@
-import connection from "../database";
+import connection from "../database.js";
 
 export async function setCategories(req, res) {
-  await connection.query(`
-  INSERT INTO
-    categories (name)
-    VALUES
-    `, [req.body]);
+  try {
+    await connection.query(`
+    INSERT INTO
+      categories (name)
+      VALUES ($1)
+      `, [req.body.name]);
 
-    res.sendStatus(201);
+      res.sendStatus(201);
+  } catch(error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+}
+
+export async function getCategories(req, res) {
+  try {
+    const { rows: categories } = await connection.query(`
+    SELECT * FROM categories`);
+
+    res.send(categories);
+  } catch(error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
 }

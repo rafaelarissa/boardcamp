2cd1e12c3eb0ae80d5fd897cd9e9aab2cfab749e
carvares <carvares.dev@gmail.com>
2022-03-11 10:33:10 -0300

    preparando review

--- src/routes/categoriesRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCategories, setCategories } from "../controllers/categoriesController.js";
-import { categoriesValidation } from "../middlewares/validateSchemaCategories.js";
-
-const categoriesRouter = Router();
-categoriesRouter.post('/categories', categoriesValidation, setCategories);
-categoriesRouter.get('/categories', getCategories);
-
-export default categoriesRouter;
